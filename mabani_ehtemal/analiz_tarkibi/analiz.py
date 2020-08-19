c6ed82fd780e137471ff17cd9553a8c91b1eba36
Arya Ebrahimi <58482239+Ahsok4@users.noreply.github.com>
2020-08-19 08:20:59 +0430

    Add files via upload

---
+++ mabani_ehtemal/analiz_tarkibi/analiz.py
@@ -0,0 +1,16 @@
+# final answers:
+a1 = 10*9*8*7*6*5*4
+a2 = 9*9*8*7*6*5*4
+a3 = a1 - 7*6*5*4
+
+# Calculate prob2:
+prob2 = "n(n-1)!"
+
+
+# Calculate x2:
+x2 = 165
+# Calculate x3:
+x3 = 140
+
+# Calculate x4:
+x4 = 3*14*13*11 + 6*13*14 + 14 + 24*13*11
